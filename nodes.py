9cd645cb4c2b4873c6ac2c412e0722c0f102bafb
Isi <isinsemail@gmail.com>
2025-04-05 11:41:07 +0100

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -22,7 +22,7 @@ import comfy.model_management as model_management
 import comfy.utils
 import folder_paths
 
-import scripts.reactor_version
+# import scripts.reactor_version
 from r_chainner import model_loading
 from scripts.reactor_faceswap import (
     FaceSwapScript,
