77eab84acd884c6d45c7b5e3c5f8165bbdfca97c
Isi <isinsemail@gmail.com>
2025-04-05 14:45:26 +0100

    Update __init__.py

--- r_basicsr/archs/__init__.py
+++ r_basicsr/archs/__init__.py
@@ -2,8 +2,9 @@ import importlib
 from copy import deepcopy
 from os import path as osp
 
-from ..r_basicsr.utils import get_root_logger, scandir
-from ..r_basicsr.utils.registry import ARCH_REGISTRY
+# from ..r_basicsr.utils import get_root_logger, scandir
+from ..utils import get_root_logger, scandir
+from ..utils.registry import ARCH_REGISTRY
 
 __all__ = ['build_network']
 
