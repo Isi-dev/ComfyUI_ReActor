b8816a4e58553bba98035e6da039482c55d789a2
Isi <isinsemail@gmail.com>
2025-04-07 16:02:14 +0100

    Update __init__.py

--- r_basicsr/__init__.py
+++ r_basicsr/__init__.py
@@ -1,7 +1,7 @@
 # https://github.com/xinntao/BasicSR
 # flake8: noqa
-from .archs import *
-from .data import *
+# from .archs import *
+# from .data import *
 # from .losses import *
 # from .metrics import *
 # from .models import *
